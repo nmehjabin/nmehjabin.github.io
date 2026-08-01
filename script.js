a691fd544c8946c8333d076f00de9521331457af
Nadia Mehjabin <nmehjabin55@gmail.com>
2026-08-01 18:00:46 -0400

    Add toggle functionality for navigation links

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// Grab the button and the nav links container
+const toggleButton = document.querySelector('#nav-toggle');
+const navLinks = document.querySelector('#nav-links');
 
+// When the button is clicked, toggle the 'open' class on the nav links
+toggleButton.addEventListener('click', function() {
+  navLinks.classList.toggle('open');
+});
